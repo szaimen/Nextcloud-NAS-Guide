0593fd22cdeafb96a7995192bcdd298e52ae725b
szaimen <szaimen@e.mail.de>
2021-01-04 16:46:50 +0100

    fix website description

--- src/pages/index.js
+++ src/pages/index.js
@@ -74,8 +74,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Instructions how to set up a home server">
       <header className={clsx('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
