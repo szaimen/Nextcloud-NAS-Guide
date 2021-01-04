54f997fe1cb848e8ebac2ca67f36cc87f5918547
szaimen <szaimen@e.mail.de>
2021-01-05 00:54:38 +0100

    revert trying to fix bad looking button

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,7 +83,7 @@ function Home() {
           <div className={styles.buttons}>
             <Link
               className={clsx(
-                'button button--outline button-secondary button--lg',
+                'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
               to={useBaseUrl('docs/')}>
