dd5d50fe6e0cdcf27e1106956ce01d9b4c783ac6
szaimen <szaimen@e.mail.de>
2021-05-01 20:03:35 +0200

    Update check_links.sh

--- check_links.sh
+++ check_links.sh
@@ -6,14 +6,15 @@ echo '#####################################################'
 mapfile -t LINKS <<< "$LINKS"
 for link in "${LINKS[@]}"
 do
-    if echo "$link" | grep -q "yourdomain\|geizhals.eu\|192.168.178.144\|internal.*address\|packages.cisofy.com"
-    then
+    if echo "$link" | grep -q "yourdomain\|geizhals.eu\|192.168.178.144\|internal.*address\|packages.cisofy.com"; then
         echo "Not testing $link"
         continue
     fi
     if [ "$(curl -sLI "$link" -o /dev/null -w "%{http_code}\n")" != 200 ]; then
         echo "Invalid is: $link"
         INVALID=1
+    else
+        echo "Valid is: $link"
     fi
 done
 if [ -n "$INVALID" ]; then
