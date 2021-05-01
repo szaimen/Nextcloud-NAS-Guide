add89ce7141e8f00ba5d6b820d5e9d3835b3a14a
szaimen <szaimen@e.mail.de>
2021-05-01 20:23:37 +0200

    Update check_links.sh

--- check_links.sh
+++ check_links.sh
@@ -9,11 +9,14 @@ do
     fi
     if [ "$(curl -sLI "$link" -o /dev/null -w "%{http_code}\n")" != 200 ]; then
         echo "Invalid is: $link"
-        INVALID=1
+        INVALID_LINKS+="$link\n"
     else
         echo "Valid is: $link"
     fi
 done
-if [ -n "$INVALID" ]; then
+if [ -n "$INVALID_LINKS" ]; then
+    echo '################################'
+    echo 'Invalid Links:'
+    echo -e "$INVALID_LINKS"
     exit 1
 fi
